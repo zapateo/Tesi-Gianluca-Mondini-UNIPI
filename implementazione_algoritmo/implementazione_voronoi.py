aecfc99622319df69a3c45e7e22f689b7775c1e6
Gianluca Mondini <mimmi@localhost.localdomain>
2018-08-11 17:50:01 +0200

    Modifiche varie all'algoritmo

--- implementazione_algoritmo/implementazione_voronoi.py
+++ implementazione_algoritmo/implementazione_voronoi.py
@@ -11,6 +11,8 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
 
 class Edge:
     """
@@ -63,7 +65,14 @@ def segment_slope(edge):
     """
     dy = edge.end.y - edge.start.y
     dx = edge.end.x - edge.start.x
-    return dy/dx
+    if dx == 0 and dy > 0:
+        return float("inf")
+    elif dx == 0 and dy < 0:
+        return float("inf")
+    elif dx == 0 and dy == 0:
+        return float("Nan")
+    else:
+        return dy/dx
 
 def perpendicular_bisector(edge):
     """
@@ -83,12 +92,17 @@ def perpendicular_bisector(edge):
 
 def y_intercept_of_segment(edge):
     """
-    Restituisce il termine noto del segmento `edge`
+    Restituisce il termine noto del segmento `edge`,
+    oppure `-9999999` nel caso in cui il segmento sia verticale
 
     >>> y_intercept_of_segment(Edge(Point(1, 1), Point(2, 2)))
     0.0
     >>> y_intercept_of_segment(Edge(Point(0, 1), Point(1, 2)))
     1.0
+    >>> y_intercept_of_segment(Edge(Point(2, 2), Point(2, 50)))
+    -9999999
+    >>> y_intercept_of_segment(Edge(Point(-2, -4), Point(-2, 22)))
+    -9999999
     """
 
     x1 = edge.start.x
@@ -97,7 +111,9 @@ def y_intercept_of_segment(edge):
     y2 = edge.end.y
 
     if x2 == x1:
-        raise Exception("Unable to compute the y-intercept of a vertical segment")
+        # raise Exception("Unable to compute the y-intercept of a vertical segment")
+        # return float("NaN")
+        return -9999999
 
     q = (x2 * y1 - x1 * y2)/(x2 - x1)
     return q
@@ -118,6 +134,9 @@ def lines_intersection(line1, line2):
     b = line2.m
     d = line2.q
 
+    if (a - b) == 0:
+        return None
+
     x = (d - c)/(a - b)
     y = a * x + c
     return Point(x, y)
@@ -129,6 +148,12 @@ def from_segment_to_line(edge):
     m = segment_slope(edge)
     q = y_intercept_of_segment(edge)
     return Line(m, q)
+
+def lines_distance(line1, line2):
+    if line1.m != line2.m:
+        return 0
+    else:
+        return abs(line1.q - line2.q)
 
 class Draw:
     @staticmethod
@@ -171,13 +196,44 @@ def compute_voronoi(S, width, height):
     # Step 2
     # Aggiunta di 3 o 4 "punti all'infinito" alla lista C (cells),
     # per delimitare il diagramma
+    infinity=3
     C.append(
         Cell(
             Point(-width, -height),
             [
-                Edge(Point(width/2, height/2), Point(width/2, -10*height)),
-                Edge(Point(width/2, -10*height), Point(-10*width, height/2)),
-                Edge(Point(-10*width, height/2), Point(width/2, height/2)),
+                Edge(Point(0, 0), Point(0, -infinity*height)),
+                Edge(Point(0, -infinity*height), Point(-infinity*width, 0)),
+                Edge(Point(-infinity*width, 0), Point(0, 0)),
+            ]
+        )
+    )
+    C.append(
+        Cell(
+            Point(width, -height),
+            [
+                Edge(Point(0, 0), Point(0, -infinity*height)),
+                Edge(Point(0, -infinity*height), Point(infinity*width, 0)),
+                Edge(Point(infinity*width, 0), Point(0, 0))
+            ]
+        )
+    )
+    C.append(
+        Cell(
+            Point(width, height),
+            [
+                Edge(Point(0, 0), Point(infinity*width, 0)),
+                Edge(Point(infinity*width, 0), Point(0, infinity*height)),
+                Edge(Point(0, infinity*height), Point(0, 0)),
+            ]
+        )
+    )
+    C.append(
+        Cell(
+            Point(-width, height),
+            [
+                Edge(Point(0, 0), Point(0, infinity*height)),
+                Edge(Point(0, infinity*height), Point(-infinity*width, 0)),
+                Edge(Point(-infinity*width, 0), Point(0, 0)),
             ]
         )
     )
@@ -206,6 +262,22 @@ def compute_voronoi(S, width, height):
 
                 # Step 9
                 # Calcola la distanza tra il segmento `e` e la linea retta `pb`
+                e_to_pb_distance = lines_distance(
+                    from_segment_to_line(e),
+                    pb
+                )
+
+                # Step 10
+                pass
+
+                # Step 11
+                # Se il segmento `e` interseca la retta `pb`, taglia e nel lato più lontano di `pb`, e memorizza il punto di intersezione in X
+                X.append(
+                    lines_intersection(
+                        from_segment_to_line(e),
+                        pb
+                    )
+                )
 
             # Step 12
             # In questo momento X dovrebbe contenere 0 o 2 punti; se ne ha 2, crea un nuovo Edge che li connetta, e aggiungi questo nuovo `new_edge` a `c`, `cell` e `E`
@@ -214,11 +286,14 @@ def compute_voronoi(S, width, height):
                 c.edges.append(new_edge)
                 cell.edges.append(new_edge)
                 E.append(new_edge)
+            elif len(X) == 0:
+                pass
+            else:
+                raise Exception("X should contain 0 or 2 elements. X: " + ", ".join([str(x) for x in X]))
 
             # Step 13
             # Se necessario, cancella ogni segmento marcato nello step 10 sia da `c` che da `E`
 
-compute_voronoi([Point(-3, -4), Point(0, 2), Point(1, 3), Point(-3, 3)], 10, 20)
     screen.fill((255, 255, 255))
     for edge in E:
         Draw.edge(edge)
@@ -233,6 +308,8 @@ screen = pygame.display.set_mode(screen_size)
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+    compute_voronoi([Point(-3, -4), Point(0, 2), Point(1, 3), Point(-3, 3)], int(screen_size[0]/4-40), int(screen_size[1]/4-40))
 
     while True:
         for event in pygame.event.get():

b4c534d67f38fc54e55adcf1c7c8e063ba935ea2
Gianluca Mondini <mimmi@localhost.localdomain>
2018-08-20 10:45:48 +0200

    Le classi e le funz. geom. sono state spostate in un file dedicato

--- implementazione_algoritmo/implementazione_voronoi_python/implementazione_voronoi.py
+++ implementazione_algoritmo/implementazione_voronoi_python/implementazione_voronoi.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
-import math
 from warnings import warn
+
+from geometria import *
 
 ENABLED_DEBUG = True
 
@@ -10,320 +11,6 @@ def debug(msg):
         print(f"[DEBUG] {msg}")
 
 # Può essere utile: https://www.cs.hmc.edu/ACM/lectures/convex_hull.html
-
-class Point:
-    """
-    Un generico punto nello spazio bidimensionale
-
-    Un sito è un particolare tipo di punto che definisce
-    il diagramma di Voronoi
-
-    Utilizzo:
-
-    >>> p1 = Point(43.3, -11.89)
-    >>> p1.x
-    43.3
-    >>> p1.y
-    -11.89
-    """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"Point({self.x}, {self.y})"
-
-class Edge:
-    """
-    Un bordo, composto da un punto iniziale e da un punto finale
-    """
-    def __init__(self, start, end):
-        assert type(start) == Point
-        assert type(end) == Point
-        self.start = start
-        self.end = end
-        self.to_be_deleted = False # Usato nell'algoritmo di Voronoi
-
-    def mark_to_be_deleted(self):
-        self.to_be_deleted = True
-
-    def __str__(self):
-        return f"Edge_from:{self.start}to:{self.end}"
-
-class Cell:
-    """
-    Una cella di Voronoi, composta da un sito (= punto caratterizzante il diagramma di Voronoi) e da una lista di bordi
-    """
-    def __init__(self, site, edges):
-        self.site = site
-        self.edges = edges
-
-class Line_abc:
-    """
-    Una linea retta rappresentata nella forma
-    ax + by + c = 0
-    """
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-def point_to_point_distance(p1, p2):
-    """
-    Restituisce la distanza tra i punti `p1` e `p2`
-
-    >>> point_to_point_distance(Point(1, 1), Point(2, 2))
-    1.4142135623730951
-
-    >>> point_to_point_distance(Point(0, 3), Point(0, -1))
-    4.0
-    """
-    dx = p1.x - p2.x
-    dy = p1.y - p2.y
-    return math.sqrt(dx**2 + dy**2)
-
-
-
-def perpendicular_bisector(edge):
-    """
-    Restituisce la linea retta bisettrice di "edge"
-
-    >>> pb = perpendicular_bisector(Edge(Point(0, 2), Point(2, 0)))
-    >>> pb.a
-    -1.0
-    >>> pb.b
-    1
-    >>> pb.c
-    -0.0
-
-    >>> pb = perpendicular_bisector(Edge(Point(5, -3), Point(5, 2)))
-    >>> pb.a
-    0
-    >>> pb.b
-    1
-    >>> pb.c
-    0.5
-
-    >>> pb = perpendicular_bisector(Edge(Point(-15, 2), Point(-15, -3)))
-    >>> pb.a
-    0
-    >>> pb.b
-    1
-    >>> pb.c
-    0.5
-    """
-    p = midpoint(edge)
-    m1 = segment_slope(edge)
-    if not m1: # edge è verticale
-        # a·x + b·y + c = 0
-        neg_c = (edge.start.y + edge.end.y)/2
-        return Line_abc(0, 1, -neg_c)
-    else: # edge non è verticale
-        m2 = -1/m1
-        q = - m2 * p.x + p.y
-        return Line_abc(-m2, 1, -q)
-
-def segment_slope(edge):
-    """
-    Restituisce il coefficiente angolare del segmento `edge`:
-
-    >>> segment_slope(Edge(Point(0, 0), Point(10, 10)))
-    1.0
-    >>> segment_slope(Edge(Point(0, 2), Point(2, 0)))
-    -1.0
-
-    ..oppure `None` nel caso in cui il segmento sia verticale:
-
-    >>> segment_slope(Edge(Point(1, 10), Point(1, -2)))
-    >>> segment_slope(Edge(Point(-5, 10), Point(-5, -2)))
-    """
-    dy = edge.end.y - edge.start.y
-    dx = edge.end.x - edge.start.x
-    if dx == 0:
-        return None
-    else:
-        return dy/dx
-
-def midpoint(edge):
-    """
-    Restituisce il punto medio del segmento "edge"
-
-    >>> p = midpoint(Edge(Point(0, 0), Point(2, 2)))
-    >>> p.x
-    1.0
-    >>> p.y
-    1.0
-
-    >>> p = midpoint(Edge(Point(1, 1), Point(4, -5)))
-    >>> p.x
-    2.5
-    >>> p.y
-    -2.0
-    """
-    xm = (edge.start.x + edge.end.x)/2
-    ym = (edge.start.y + edge.end.y)/2
-    return Point(xm, ym)
-
-def from_line_to_segment(line):
-    """
-    Restituisce un segmento che giace sulla retta `line`
-
-    >>> edge = from_line_to_segment(Line_abc(1, 3, -2))
-    >>> print(edge.start)
-    Point(-1000, 334.0)
-    >>> print(edge.end)
-    Point(1000, -332.66666666666663)
-    """
-    big = 1000
-
-    a, b, c = line.a, line.b, line.c
-
-    # Caso in cui la retta sia verticale
-    if b == 0 and a != 0:
-        raise Exception("[TODO] retta verticale non gestita")
-
-    m = -a/b
-    q = -c/b
-
-    f = lambda x: m*x + q
-
-    p1 = Point(-big, f(-big))
-    p2 = Point(+big, f(+big))
-
-    return Edge(p1, p2)
-
-def segment_intersection(edge1, edge2):
-    """
-    Calcola il punto di intersezione tra il segmento `edge1` e `edge2`
-
-    Restituisce `None` nel caso in cui non esista un punto di intersezione,
-    oppure un oggetto di tipo `Point`
-
-    Adattato dal codice presente su https://www.cs.hmc.edu/ACM/lectures/intersections.html
-
-    >>> i = segment_intersection(Edge(Point(0, 0), Point(2, 2)), Edge(Point(0, 2), Point(2, 0)))
-    >>> i.x
-    1.0
-    >>> i.y
-    1.0
-
-    >>> i = segment_intersection(Edge(Point(1, 1), Point(4, -5)), Edge(Point(2, -3), Point(3, -1)))
-    >>> i.x
-    2.5
-    >>> i.y
-    -2.0
-
-    Nel caso in cui non è presente alcun punto di intersezione, viene restituito `None`
-
-    >>> segment_intersection(Edge(Point(0, 0), Point(0, 10)), Edge(Point(2, 0), Point(2, -10)))
-    >>> segment_intersection(Edge(Point(-2, 3), Point(3, 3)), Edge(Point(9, 6), Point(9, 2)))
-    >>> segment_intersection(Edge(Point(0, 1), Point(1, 2)), Edge(Point(2, 2), Point(1, 3)))
-    """
-
-    DET_TOLERANCE = 0.00000001
-
-    pt1 = (edge1.start.x, edge1.start.y)
-    pt2 = (edge1.end.x,   edge1.end.y)
-
-    ptA = (edge2.start.x, edge2.start.y)
-    ptB = (edge2.end.x,   edge2.end.y)
-
-    # the first line is pt1 + r*(pt2-pt1)
-    # in component form:
-    x1, y1 = pt1;   x2, y2 = pt2
-    dx1 = x2 - x1;  dy1 = y2 - y1
-
-    # the second line is ptA + s*(ptB-ptA)
-    x, y = ptA;   xB, yB = ptB;
-    dx = xB - x;  dy = yB - y;
-
-    # we need to find the (typically unique) values of r and s
-    # that will satisfy
-    #
-    # (x1, y1) + r(dx1, dy1) = (x, y) + s(dx, dy)
-    #
-    # which is the same as
-    #
-    #    [ dx1  -dx ][ r ] = [ x-x1 ]
-    #    [ dy1  -dy ][ s ] = [ y-y1 ]
-    #
-    # whose solution is
-    #
-    #    [ r ] = _1_  [  -dy   dx ] [ x-x1 ]
-    #    [ s ] = DET  [ -dy1  dx1 ] [ y-y1 ]
-    #
-    # where DET = (-dx1 * dy + dy1 * dx)
-    #
-    # if DET is too small, they're parallel
-    #
-    DET = (-dx1 * dy + dy1 * dx)
-
-    if math.fabs(DET) < DET_TOLERANCE:
-        # return (0,0,0,0,0)
-        return None
-
-    # now, the determinant should be OK
-    DETinv = 1.0/DET
-
-    # find the scalar amount along the "self" segment
-    r = DETinv * (-dy  * (x-x1) +  dx * (y-y1))
-
-    # find the scalar amount along the input line
-    s = DETinv * (-dy1 * (x-x1) + dx1 * (y-y1))
-
-    # return the average of the two descriptions
-    xi = (x1 + r*dx1 + x + s*dx)/2.0
-    yi = (y1 + r*dy1 + y + s*dy)/2.0
-    # return ( xi, yi, 1, r, s )
-
-    # print(f"r = {r}, s = {s}, xi = {xi}, yi = {yi}")
-
-    if 0 < r < 1 and 0 < s < 1:
-        return Point(xi, yi)
-    else:
-        return None
-
-def point_segment_distance(point, edge):
-    """
-    Restituisce la distanza minima tra il punto `point` ed il segmento `edge`
-
-    >>> point_segment_distance(Point(5, 3), Edge(Point(0, 1), Point(5, 1)))
-    2.0
-    >>> point_segment_distance(Point(8, 1), Edge(Point(0, 1), Point(5, 1)))
-    3.0
-    >>> point_segment_distance(Point(0, 1), Edge(Point(0, 1), Point(5, 1)))
-    0.0
-    >>> point_segment_distance(Point(-1, 2), Edge(Point(0, 1), Point(5, 1)))
-    1.4142135623730951
-
-    Nel caso in cui il segmento abbia una lunghezza nulla, restituisce 0.0
-    >>> point_segment_distance(Point(-43, 33.3), Edge(Point(34, -3), Point(34, -3)))
-    0.0
-    """
-
-    # Codice adattato da https://stackoverflow.com/a/49504330
-
-    dx = edge.end.x - edge.start.x
-    dy = edge.end.y - edge.start.y
-    dr2 = float(dx ** 2 + dy ** 2)
-
-    if dr2 == 0:
-        return 0.0
-
-    lerp = ((point.x - edge.start.x) * dx + (point.y - edge.start.y) * dy) / dr2
-    if lerp < 0:
-        lerp = 0
-    elif lerp > 1:
-        lerp = 1
-
-    x = lerp * dx + edge.start.x
-    y = lerp * dy + edge.start.y
-
-    _dx = x - point.x
-    _dy = y - point.y
-    square_dist = _dx ** 2 + _dy ** 2
-
-    return math.sqrt(square_dist)
 
 class Draw:
     @staticmethod
@@ -361,11 +48,11 @@ def compute_voronoi(S, width, height):
         Draw.point(site)
 
 
-    # Step 1
+    # NOTE: Step 1
     E = [] # lista di bordi, "edges"
     C = [] # lista di celle
 
-    # Step 2
+    # NOTE: Step 2
     # Aggiunta di 3 o 4 "punti all'infinito" alla lista C (cells),
     # per delimitare il diagramma
     infinity=3
@@ -410,44 +97,47 @@ def compute_voronoi(S, width, height):
         )
     )
 
-    # Step 3
+    # NOTE: Step 3
+    debug(f"'for site in S' is going to iterate {len(S)} times")
     for site in S:
 
-        # Step 4
+        # NOTE: Step 4
         # Crea una nuova cella "cell", che abbia "site" come suo sito
         cell = Cell(site, [])
 
-        # Step 5
+        # NOTE: Step 5
+        debug(f"'for c in C' is going to iterate {len(C)} times")
         for c in C:
 
-            # Step 6
+            # NOTE: Step 6
             # Trova la bisettrice del segmento che unisce `site` e `c.site` e chiamala `pb`
             pb = perpendicular_bisector(Edge(site, c.site))
             assert any([pb.a != 0, pb.b != 0, pb.c != 0])
 
-            # Step 7
+            # NOTE: Step 7
             # Crea una struttura dati X che andrà a contenere i punti critici dell'algoritmo
             X = []
 
-            # Step 8
+            # NOTE: Step 8
             # Per ogni edge `e` in `c`
+            debug(f"'for e in c.edges' is going to iterate {len(c.edges)} times")
             for e in c.edges:
 
-                # Step 9
+                # NOTE: Step 9
                 # Calcola la distanza tra il segmento `e` e la linea retta `pb`
                 # e_to_pb_distance = lines_distance(
                 #     from_segment_to_line(e),
                 #     pb
                 # )
 
-                # Step 10
+                # NOTE: Step 10
                 # Se il segmento `e` è sul lato vicino della retta bisettrice `pb` (ovvero se è più vicino al punto `site` rispetto che al punto `c.site`), contrassegna `e` per la cancellazione (oppure cancellalo subito se questo non va a danneggiare l'enumerazione)
                 e_site_distance = point_segment_distance(site, e)
                 e_c_site_distance = point_segment_distance(c.site, e)
                 if e_site_distance < e_c_site_distance:
                     e.mark_to_be_deleted()
 
-                # Step 11
+                # NOTE: Step 11
                 # Se il segmento `e` interseca la retta `pb`, taglia e nel lato più lontano di `pb`, e memorizza il punto di intersezione in X
                 e_pb_intersection = segment_intersection(
                     e,
@@ -456,7 +146,7 @@ def compute_voronoi(S, width, height):
                 if e_pb_intersection:
                     X.append(e_pb_intersection)
 
-            # Step 12
+            # NOTE: Step 12
             # In questo momento X dovrebbe contenere 0 o 2 punti; se ne ha 2, crea un nuovo Edge che li connetta, e aggiungi questo nuovo `new_edge` a `c`, `cell` e `E`
             if len(X) == 2:
                 new_edge = Edge(X[0], X[1])
@@ -466,9 +156,9 @@ def compute_voronoi(S, width, height):
             elif len(X) == 0:
                 pass
             else:
-                warn("X should contain 0 or 2 elements. X: " + ", ".join([str(x) for x in X]))
+                debug(f"X should contain 0 or 2 elements, but it contains {len(X)} items. X: " + ", ".join([str(x) for x in X]))
 
-            # Step 13
+            # NOTE: Step 13
             # Se necessario, cancella ogni segmento marcato nello step 10 sia da `c` che da `E`
 
             for cell in C:
@@ -478,21 +168,34 @@ def compute_voronoi(S, width, height):
             # E = list(filter(lambda e: not e.to_be_deleted, E))
             # assert len(E) < prev_E_len, "No edges have been deleted from E"
 
+    # Disegno gli assi cartesiani
+    # pygame.draw.line(screen, reference_line_color, (0, screen_size[1]/2), (screen_size[0], screen_size[1]/2), 1)
+    # pygame.draw.line(screen, reference_line_color, (screen_size[0]/2, 0), (screen_size[0]/2, screen_size[1]), 1)
+
+    # Disegno il perimetro del rettangolo
+    ref_line_color = (0, 0, 255)
+    pygame.draw.line(screen, ref_line_color, (-width + screen_size[0]/2, -height + screen_size[1]/2), (width + screen_size[0]/2, -height + screen_size[1]/2))
+    pygame.draw.line(screen, ref_line_color, (-width + screen_size[0]/2, height+screen_size[1]/2), (-width + screen_size[0]/2, -height + screen_size[1]/2))
+    pygame.draw.line(screen, ref_line_color, (width + screen_size[0]/2, -height+screen_size[1]/2), (width + screen_size[0]/2, height + screen_size[1]/2))
+    pygame.draw.line(screen, ref_line_color, (width + screen_size[0]/2, height+screen_size[1]/2), (-width + screen_size[0]/2, height + screen_size[1]/2))
+
     for edge in E:
         Draw.edge(edge)
     for cell in C:
         Draw.cell(cell)
 
-screen_size = 800, 600
+
+screen_size = 1000, 800
 pygame.init()
 screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Implementazione Voronoi - Gianluca Mondini")
 screen.fill((255, 255, 255))
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
 
-    compute_voronoi([Point(-20, -40), Point(0, 20), Point(100, 3), Point(-3, 3)], int(screen_size[0]/4-40), int(screen_size[1]/4-40))
+    compute_voronoi([Point(-20, -40), Point(0, 20), Point(100, 3), Point(-3, 3)], int(screen_size[0]/4-30), int(screen_size[1]/4-30))
 
     while True:
         for event in pygame.event.get():

2913bb852979dbd1a6d8b178e781fd7839c94c09
Gianluca Mondini <mimmi@localhost.localdomain>
2018-08-26 16:15:56 +0200

    Le funzioni di utilità sono state spostate nel file utilities.py

---
+++ implementazione_algoritmo/implementazione_voronoi_python_ottimizzato/utilities.py
@@ -0,0 +1,110 @@
+import pygame
+import sys
+
+from geometria import *
+
+ENABLED_DEBUG = True
+def debug(msg):
+    if ENABLED_DEBUG:
+        print(f"[DEBUG] {msg}")
+
+class Drawer:
+
+    def __init__(self, screen, screen_width, screen_height):
+        self.screen = screen
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.clear()
+        self.font = pygame.font.SysFont("freemono", 12)
+
+    def title(self, t):
+        pygame.display.set_caption(str(t))
+
+    def _write_text(self, text, pos):
+        """
+        `pos` must be in cartesian form
+        """
+        surface = self.font.render(text, True, (0, 0, 0))
+        self.screen.blit(surface, self._cart_to_screen(pos))
+        pygame.display.flip()
+
+    def clear(self):
+        self.screen.fill((255, 255, 255))
+        pygame.display.set_caption("Gianluca Mondini - Voronoj")
+
+    def wait(self):
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                return
+
+    def draw_all(self, shapes):
+        for shape in shapes:
+            self.draw(shape)
+
+    def _cart_to_screen(self, pos):
+        return (
+            int(pos[0] + self.screen_width/2),
+            int(self.screen_height - pos[1] - self.screen_height/2)
+        )
+
+    def draw(self, shape, annotation=None):
+        if type(shape) == Point:
+            pygame.draw.circle(
+                self.screen,
+                (255, 0, 0),
+                self._cart_to_screen(
+                    (shape.x, shape.y)
+                ),
+                5
+            )
+            if annotation:
+                self._write_text(annotation, (shape.x, shape.y))
+        elif type(shape) == Edge:
+            line_color = (130, 130, 130,)
+            pygame.draw.line(
+                self.screen,
+                line_color,
+                self._cart_to_screen((shape.start.x, shape.start.y)),
+                self._cart_to_screen((shape.end.x, shape.end.y))
+            )
+            pygame.draw.circle(
+                self.screen,
+                line_color,
+                self._cart_to_screen(
+                    (shape.start.x, shape.start.y)
+                ),
+                2
+            )
+            # shape.start.x = round(shape.start.x, 1)
+            # shape.start.y = round(shape.start.y, 1)
+            # shape.end.x = round(shape.end.x, 1)
+            # shape.end.y = round(shape.end.y, 1)
+            # self._write_text(str((shape.start.x, shape.start.y)), (shape.start.x, shape.start.y))
+            pygame.draw.circle(
+                self.screen,
+                line_color,
+                self._cart_to_screen(
+                    (shape.end.x, shape.end.y)
+                ),
+                2
+            )
+            # self._write_text(str((shape.end.x, shape.end.y)), (shape.end.x, shape.end.y))
+            if annotation:
+                x = int((shape.start.x + shape.end.x)/2)
+                y = int((shape.start.y + shape.end.y)/2)
+                self._write_text(annotation, ((x, y)))
+                pygame.draw.circle(
+                    self.screen,
+                    (255, 0, 0),
+                    self._cart_to_screen((x, y)),
+                    3
+                )
+        elif type(shape) == Line:
+            pass
+        else:
+            raise Exception(f"Unknow shape {shape} of type {type(shape)}")
+        pygame.display.flip()
--- implementazione_algoritmo/implementazione_voronoi_python_ottimizzato/voronoj.py
+++ implementazione_algoritmo/implementazione_voronoi_python_ottimizzato/voronoj.py
@@ -2,113 +2,21 @@ import pygame
 import sys
 
 from geometria import *
+from utilities import *
 
 ENABLED_DEBUG = True
 
-def debug(msg):
-    if ENABLED_DEBUG:
-        print(f"[DEBUG] {msg}")
-
-class Drawer:
-
-    def __init__(self, screen, screen_width, screen_height):
-        self.screen = screen
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.clear()
-        self.font = pygame.font.SysFont("freemono", 12)
-
-    def title(self, t):
-        pygame.display.set_caption(str(t))
-
-    def _write_text(self, text, pos):
-        """
-        `pos` must be in cartesian form
-        """
-        surface = self.font.render(text, True, (0, 0, 0))
-        self.screen.blit(surface, self._cart_to_screen(pos))
-        pygame.display.flip()
-
-    def clear(self):
-        self.screen.fill((255, 255, 255))
-        pygame.display.set_caption("Gianluca Mondini - Voronoj")
-
-    def wait(self):
-        while True:
-            event = pygame.event.wait()
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                return
-
-    def draw_all(self, shapes):
-        for shape in shapes:
-            self.draw(shape)
-
-    def _cart_to_screen(self, pos):
-        return (
-            int(pos[0] + self.screen_width/2),
-            int(self.screen_height - pos[1] - self.screen_height/2)
-        )
-
-    def draw(self, shape, annotation=None):
-        if type(shape) == Point:
-            pygame.draw.circle(
-                self.screen,
-                (255, 0, 0),
-                self._cart_to_screen(
-                    (shape.x, shape.y)
-                ),
-                5
-            )
-            if annotation:
-                self._write_text(annotation, (shape.x, shape.y))
-        elif type(shape) == Edge:
-            line_color = (130, 130, 130,)
-            pygame.draw.line(
-                self.screen,
-                line_color,
-                self._cart_to_screen((shape.start.x, shape.start.y)),
-                self._cart_to_screen((shape.end.x, shape.end.y))
-            )
-            pygame.draw.circle(
-                self.screen,
-                line_color,
-                self._cart_to_screen(
-                    (shape.start.x, shape.start.y)
-                ),
-                2
-            )
-            shape.start.x = round(shape.start.x, 1)
-            shape.start.y = round(shape.start.y, 1)
-            shape.end.x = round(shape.end.x, 1)
-            shape.end.y = round(shape.end.y, 1)
-            self._write_text(str((shape.start.x, shape.start.y)), (shape.start.x, shape.start.y))
-            pygame.draw.circle(
-                self.screen,
-                line_color,
-                self._cart_to_screen(
-                    (shape.end.x, shape.end.y)
-                ),
-                2
-            )
-            self._write_text(str((shape.end.x, shape.end.y)), (shape.end.x, shape.end.y))
-            if annotation:
-                x = int((shape.start.x + shape.end.x)/2)
-                y = int((shape.start.y + shape.end.y)/2)
-                self._write_text(annotation, ((x, y)))
-                pygame.draw.circle(
-                    self.screen,
-                    (255, 0, 0),
-                    self._cart_to_screen((x, y)),
-                    3
-                )
-        elif type(shape) == Line:
-            pass
-        else:
-            raise Exception(f"Unknow shape {shape} of type {type(shape)}")
-        pygame.display.flip()
+def mark_unwanted_edges(edges, primary_site):
+    # FIXME quando un bordo è stato contrassegnato interrompi l'iterazione
+    edges = edges[:]
+    for outer_i in range(len(edges)):
+        for point in (edges[outer_i].start, edges[outer_i].end):
+            for inner_edge in edges:
+                join_edge = Edge(primary_site, point)
+                intersection = segment_intersection(inner_edge, join_edge)
+                if intersection and not points_are_close(intersection, point):
+                    edges[outer_i].to_be_deleted = True
+    return edges
 
 def voronoj_cell(edges, primary_site, other_sites):
     """
@@ -123,27 +31,27 @@ def voronoj_cell(edges, primary_site, other_sites):
 
     Caso in cui l'area sia quadrata
 
-    # >>> edges = [E(0, 0, 100, 0), E(100, 0, 100, 100), E(100, 100, 0, 100), E(0, 100, 0, 0)]
-    # >>> primary_site = Point(25, 25)
-    # >>> other_sites = [Point(75, 75)]
-    # >>> cell = voronoj_cell(edges, primary_site, other_sites)
-    # >>> for edge in cell:
-    # ...     print(edge)
-    # Edge_from:Point(100.0, 0.0)to:Point(0, 0)
-    # Edge_from:Point(0.0, 100.0)to:Point(0, 0)
-    # Edge_from:Point(100.0, 0.0)to:Point(0.0, 100.0)
-    #
-    # >>> edges = [E(100, 0, 300, 0), E(300, 0, 300, 200), E(300, 200, 200, 400), E(200, 400, 0, 200), E(0, 200, 100, 0)]
-    # >>> primary_site = Point(200, 300)
-    # >>> other_sites = [Point(100, 100), Point(100, 200)]
-    # >>> cell = voronoj_cell(edges, primary_site, other_sites)
-    # >>> for edge in cell:
-    # ...     print(edge)
-    # Edge_from:Point(300, 200)to:Point(200, 400)
-    # Edge_from:Point(300.0, 125.0)to:Point(300, 200)
-    # Edge_from:Point(100.0, 300.0)to:Point(200, 400)
-    # Edge_from:Point(250.0, 150.0)to:Point(300.0, 125.0)
-    # Edge_from:Point(100.0, 300.0)to:Point(250.0, 150.0)
+    >>> edges = [E(0, 0, 100, 0), E(100, 0, 100, 100), E(100, 100, 0, 100), E(0, 100, 0, 0)]
+    >>> primary_site = Point(25, 25)
+    >>> other_sites = [Point(75, 75)]
+    >>> cell = voronoj_cell(edges, primary_site, other_sites)
+    >>> for edge in cell:
+    ...     print(edge)
+    Edge_from:Point(100.0, 0.0)to:Point(0, 0)
+    Edge_from:Point(0.0, 100.0)to:Point(0, 0)
+    Edge_from:Point(100.0, 0.0)to:Point(0.0, 100.0)
+
+    >>> edges = [E(100, 0, 300, 0), E(300, 0, 300, 200), E(300, 200, 200, 400), E(200, 400, 0, 200), E(0, 200, 100, 0)]
+    >>> primary_site = Point(200, 300)
+    >>> other_sites = [Point(100, 100), Point(100, 200)]
+    >>> cell = voronoj_cell(edges, primary_site, other_sites)
+    >>> for edge in cell:
+    ...     print(edge)
+    Edge_from:Point(300, 200)to:Point(200, 400)
+    Edge_from:Point(300.0, 125.0)to:Point(300, 200)
+    Edge_from:Point(100.0, 300.0)to:Point(200, 400)
+    Edge_from:Point(250.0, 150.0)to:Point(300.0, 125.0)
+    Edge_from:Point(100.0, 300.0)to:Point(250.0, 150.0)
     """
     #---------------------------------------------------------------------------
     for site in other_sites:
@@ -156,24 +64,23 @@ def voronoj_cell(edges, primary_site, other_sites):
         #-----------------------------------------------------------------------
         new_edges = []
         #-----------------------------------------------------------------------
-        # debug("### for edge in edges:")
-        for edge in edges:
-            if edge.to_be_deleted:
-                # debug(f"ignoring {edge} cause it's going to be deleted")
+        edges = mark_unwanted_edges(edges, primary_site)
+        #-----------------------------------------------------------------------
+        # for edge in edges:
+        for i in range(len(edges)):
+            if edges[i].to_be_deleted:
                 continue
-            # debug(f"searching intersections with {edge}, edge.to_be_deleted: {edge.to_be_deleted}..")
             intersect = segment_intersection(
                 from_line_to_segment(perp_bisect),
-                edge
+                edges[i]
             )
             #-------------------------------------------------------------------
             if intersect:
-                # debug(f"found intersection at {intersect} with {edge}")
                 #---------------------------------------------------------------
-                edge.to_be_deleted = True
+                edges[i].to_be_deleted = True
                 #---------------------------------------------------------------
-                p1 = edge.start
-                p2 = edge.end
+                p1 = edges[i].start
+                p2 = edges[i].end
                 edge_p1_primary_site = Edge(p1, primary_site)
                 edge_p2_primary_site = Edge(p2, primary_site)
                 int1 = segment_intersection(
@@ -209,15 +116,7 @@ def voronoj_cell(edges, primary_site, other_sites):
                 #---------------------------------------------------------------
             # end if intersect
             #-----------------------------------------------------------------------
-            # FIXME quando un bordo è stato contrassegnato interrompi l'iterazione
-            for outer_edge in edges:
-                # debug(f"checking if {outer_edge} needs to be deleted")
-                for point in (outer_edge.start, outer_edge.end):
-                    for inner_edge in edges:
-                        join_edge = Edge(primary_site, point)
-                        intersection = segment_intersection(inner_edge, join_edge)
-                        if intersection and not points_are_close(intersection, point):
-                            outer_edge.to_be_deleted = True
+            edges = mark_unwanted_edges(edges, primary_site)
             #---------------------------------------------------------------------------
         # end for edge in edges
         edges = edges + new_edges
@@ -243,6 +142,8 @@ def voronoj_cell(edges, primary_site, other_sites):
         #-----------------------------------------------------------------------
     # end for site in other_sites
 
+    edges = mark_unwanted_edges(edges, primary_site)
+    #---------------------------------------------------------------------------
     edges = list(filter(lambda e: not e.to_be_deleted, edges))
     #---------------------------------------------------------------------------
     temp_edges = []
@@ -290,55 +191,15 @@ if __name__ == "__main__":
     import doctest
     doctest.testmod()
 
-    # sites = [Point(200, 300), Point(100, 100), Point(100, 200)]
-    # for i in range(10):
-    #     edges_to_draw = []
-    #     for primary_site in sites:
-    #         edges = [E(100, 0, 300, 0), E(300, 0, 300, 200), E(300, 200, 200, 400), E(200, 400, 0, 200), E(0, 200, 100, 0)]
-    #         other_sites = sites[:]
-    #         other_sites.remove(primary_site)
-    #         cell = voronoj_cell(edges, primary_site, other_sites)
-    #         edges_to_draw += cell
-    #         com = center_of_mass(cell)
-    #         primary_site.next_x = com.x
-    #         primary_site.next_y = com.y
-    #     drawer.clear()
-    #     drawer.draw_all(edges_to_draw)
-    #     drawer.draw_all(sites)
-    #     drawer.wait()
-    #     for site in sites:
-    #         site.x = site.next_x
-    #         site.y = site.next_y
-        # drawer.clear()
-        # drawer.draw_all(edges_to_draw)
-        # drawer.draw_all(sites)
-        # drawer.wait()
-    sites = [Point(200, 300), Point(100, 100), Point(100, 200)]
-    primary_site = sites[0]
-    other_sites = sites[:]
-    other_sites.remove(primary_site)
-    for i in range(10):
-        edges = [E(100, 0, 300, 0), E(300, 0, 300, 200), E(300, 200, 200, 400), E(200, 400, 0, 200), E(0, 200, 100, 0)]
-        cell = voronoj_cell(edges, primary_site, other_sites)
-        com = center_of_mass(cell)
-        drawer.clear()
-        drawer.draw(primary_site, "ps")
-        drawer.draw_all(cell + other_sites)
-        drawer.wait()
-        primary_site.x = com.x
-        primary_site.y = com.y
-
-
-
-    # edges = [
-    #     Edge(Point(-300, 0), Point(250, -40)),
-    #     Edge(Point(250, -40), Point(250, 250)),
-    #     Edge(Point(250, 250), Point(0, 250)),
-    #     Edge(Point(0, 250), Point(-300, 0)),
-    # ]
-    # primary_site = Point(100, 70)
-    # other_sites = [
-    #     Point(-43, 30),
-    #     Point(200, 60)
-    # ]
-    # voronoj_cell(edges, primary_site, other_sites)
+    sites = [Point(200, 300), Point(100, 100), Point(100, 200), Point(150, 200)]
+    for i in range(1):
+        for primary_site in sites:
+            edges = [E(100, 0, 300, 0), E(300, 0, 300, 200), E(300, 200, 200, 400), E(200, 400, 0, 200), E(0, 200, 100, 0)]
+            other_sites = sites[:]
+            other_sites.remove(primary_site)
+            cell = voronoj_cell(edges, primary_site, other_sites)
+            # drawer.clear()
+            drawer.draw_all(cell)
+            drawer.draw_all(sites)
+            drawer.draw(primary_site, "ps")
+            drawer.wait()

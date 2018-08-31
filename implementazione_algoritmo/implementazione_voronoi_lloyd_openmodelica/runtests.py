c6b7c8c3eed1f7fa3703f822fa1fa7e4720fdfc2
Gianluca Mondini <mimmi@localhost.localdomain>
2018-08-31 16:46:23 +0200

    Script per testare le varie classi/modelli di OpenModelica

---
+++ implementazione_algoritmo/implementazione_voronoi_lloyd_openmodelica/runtests.py
@@ -0,0 +1,31 @@
+#!/bin/env python3
+
+import re
+import subprocess
+import os
+
+FILENAME = "Geometria.mo"
+
+os.system("clear")
+
+with open(FILENAME) as f:
+    content = f.read()
+
+identifiers = re.findall(r"test_\w+", content)
+identifiers = list(set(identifiers))
+
+for i in identifiers:
+    print(f" - {i}")
+
+with open("tmp_simulate.mos", mode="w+") as f:
+    f.write(f'loadFile("{FILENAME}"); getErrorString();\n')
+    for i in identifiers:
+        f.write(f"simulate({i}); getErrorString();\n")
+
+result = subprocess.run(['omc', './tmp_simulate.mos'], stdout=subprocess.PIPE)
+result = result.stdout.decode()
+
+result = "\n".join(filter(lambda line: "time" not in line, result.split("\n")))
+
+# result = "\n".join(filter(lambda s: s.startswith("assert") and "error" in s, result.split("\n")))
+print(result)
